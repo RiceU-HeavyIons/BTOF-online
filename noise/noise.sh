911306be7ab595a4635c988408477e1c4406dbcf
fgeurts <geurts@bnl.gov>
2019-03-04 18:18:48 -0500

    Allow multiple runs to be processed from the command prompt.

--- noise/noise.sh
+++ noise/noise.sh
@@ -7,29 +7,34 @@ DAQDIR=/gpfs01/star/scratch/geurts/daq
 LOGDIR=/star/u/geurts/logs
 SUBMIT=/afs/rhic.bnl.gov/star/packages/scripts/star-submit
 
+for runnumber in $@
+do
+ echo '=== Run Number: '$runnumber' ==='
+ #runnumber=$1
 
-runnumber=$1
-run=${runnumber:0:2}
-year=$((1999+run))
-day=${runnumber:2:3}
-infile=noise$runnumber.xml
+ run=${runnumber:0:2}
+ year=$((1999+run))
+ day=${runnumber:2:3}
+ infile=noise$runnumber.xml
 
-# build input file
-echo '<?xml version="1.0" encoding="utf-8" ?>' > $WRKDIR/$infile
-echo '<job>' >> $WRKDIR/$infile
-echo '<command>' >> $WRKDIR/$infile
-echo starver $STARLIB >> $WRKDIR/$infile
-echo cd $BTOFONLINE/daqdoer >> $WRKDIR/$infile
-echo $DAQDOER $DAQDIR/$year/$day/$runnumber/'*.daq &lt;&lt; endd' >> $WRKDIR/$infile
-echo 1 >> $WRKDIR/$infile
-echo $run$day >> $WRKDIR/$infile
-echo endd >> $WRKDIR/$infile
-echo cd $BTOFONLINE/noise >> $WRKDIR/$infile
-echo "./noise -r $run$day" >> $WRKDIR/$infile
-echo '</command>' >> $WRKDIR/$infile
-echo '<stdout URL="file:'$LOGDIR/$run$day'.log" />' >> $WRKDIR/$infile
-echo '</job>' >> $WRKDIR/$infile
+ # build input file
+ echo '<?xml version="1.0" encoding="utf-8" ?>' > $WRKDIR/$infile
+ echo '<job>' >> $WRKDIR/$infile
+ echo '<command>' >> $WRKDIR/$infile
+ echo starver $STARLIB >> $WRKDIR/$infile
+ echo cd $BTOFONLINE/daqdoer >> $WRKDIR/$infile
+ echo $DAQDOER $DAQDIR/$year/$day/$runnumber/'*.daq &lt;&lt; endd' >> $WRKDIR/$infile
+ echo 1 >> $WRKDIR/$infile
+ echo $run$day >> $WRKDIR/$infile
+ echo endd >> $WRKDIR/$infile
+ echo cd $BTOFONLINE/noise >> $WRKDIR/$infile
+ echo "./noise -r $run$day" >> $WRKDIR/$infile
+ echo '</command>' >> $WRKDIR/$infile
+ echo '<stdout URL="file:'$LOGDIR/$run$day'.log" />' >> $WRKDIR/$infile
+ echo '</job>' >> $WRKDIR/$infile
 
-# execute
-cd $WRKDIR
-$SUBMIT $infile
+ # execute
+ cd $WRKDIR
+ $SUBMIT $infile
+done
+
