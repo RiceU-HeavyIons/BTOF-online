45df23fab4c8f0a2f1ec09bf945aa4194261b0d0
fgeurts <geurts@bnl.gov>
2020-09-01 11:18:53 -0400

    updated to using SUMS Sandbox and Generator elements

--- noise/noise.sh
+++ noise/noise.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+#
+# Build and submit Scheduler XML files for single noise run analysis
+#
 STARLIB=SL18f
-WRKDIR=~/scheduler
-DAQDOER=./daqdoer
+WRKDIR=~/scheduler/noise
 SUBMIT=/afs/rhic.bnl.gov/star/packages/scripts/star-submit
 [ -z "$BTOFONLINE" ] && BTOFONLINE=/star/u/geurts/BTOF-online
 [ -z  "$DAQDIR" ]    && DAQDIR=/gpfs01/star/scratch/geurts/daq
@@ -10,8 +12,8 @@ SUBMIT=/afs/rhic.bnl.gov/star/packages/scripts/star-submit
 for runnumber in $@
 do
  echo '=== Scheduling Run Number: '$runnumber' ==='
- #runnumber=$1
 
+ # deconstruct run number
  run=${runnumber:0:2}
  year=$((1999+run))
  day=${runnumber:2:3}
@@ -19,21 +21,31 @@ do
 
  # build input file
  echo '<?xml version="1.0" encoding="utf-8" ?>' > $WRKDIR/$infile
- echo '<job name="tof noise ' $runnumber'" >' >> $WRKDIR/$infile
+ echo '<job name="tof noise' $runnumber'" >' >> $WRKDIR/$infile
  echo '<command>' >> $WRKDIR/$infile
  echo starver $STARLIB >> $WRKDIR/$infile
- echo cd $BTOFONLINE/daqdoer >> $WRKDIR/$infile
- echo $DAQDOER $DAQDIR/$year/$day/$runnumber/'*.daq &lt;&lt; endd' >> $WRKDIR/$infile
+ echo ./daqdoer $runnumber/'*.daq &lt;&lt; endd' >> $WRKDIR/$infile
  echo 1 >> $WRKDIR/$infile
  echo $run$day >> $WRKDIR/$infile
  echo endd >> $WRKDIR/$infile
- echo cd $BTOFONLINE/noise >> $WRKDIR/$infile
  echo "./noise -r $run$day" >> $WRKDIR/$infile
  echo '</command>' >> $WRKDIR/$infile
+ echo '<SandBox>' >> $WRKDIR/$infile
+ echo ' <Package>' >> $WRKDIR/$infile
+ echo "  <File>file:$BTOFONLINE/daqdoer/daqdoer</File>" >> $WRKDIR/$infile
+ echo "  <File>file:$BTOFONLINE/noise/noise</File>" >> $WRKDIR/$infile
+ echo "  <File>file:$DAQDIR/$year/$day/$runnumber/</File>" >> $WRKDIR/$infile
+ echo ' </Package>' >> $WRKDIR/$infile >> $WRKDIR/$infile
+ echo '</SandBox>' >> $WRKDIR/$infile >> $WRKDIR/$infile
  echo '<stdout URL="file:'$LOGDIR/$run$day'.log" />' >> $WRKDIR/$infile
+ echo '<Generator>' >> $WRKDIR/$infile
+ echo " <Location>$WRKDIR</Location>" >> $WRKDIR/$infile
+ echo " <ReportLocation>$WRKDIR/reports</ReportLocation>" >> $WRKDIR/$infile
+ echo " <ScriptLocation>$WRKDIR/scripts</ScriptLocation>" >> $WRKDIR/$infile
+ echo " <ListLocation>$WRKDIR/lists</ListLocation>" >> $WRKDIR/$infile
+ echo '</Generator>' >> $WRKDIR/$infile
  echo '</job>' >> $WRKDIR/$infile
 
- # execute
- cd $WRKDIR
- $SUBMIT $infile
+ # submit file to SUMS
+ $SUBMIT $WRKDIR/$infile
 done
